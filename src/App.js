6904a66b5cc5a6f3c6859c9fc9902197221b025c
tanfeng95 <tanfeng95@hotmail.com>
2022-06-20 21:39:57 +0800

    add npms and route-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+import {
+  BrowserRouter, Routes,
+  Route,
+  Link, useNavigate,
+} from 'react-router-dom';
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3004';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route>
+
+      </Route>
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
     </div>
   );
 }

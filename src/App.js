4605729d1960a36b8c7b74af525087cd7f47d2c7
tanfeng95 <tanfeng95@hotmail.com>
2022-06-21 22:44:41 +0800

    added global state

--- src/App.js
+++ src/App.js
@@ -7,20 +7,18 @@ import {
   Route,
   Link, useNavigate,
 } from 'react-router-dom';
-
+import React, { useState , useEffect, useContext } from 'react';
 import Products from './components/Products';
-
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3004';
-
 
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-         <Route path='/' element={<Products/>}></Route> 
-      </Routes>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Products/>}></Route> 
+        </Routes>
+      </div>
+
   );
 }
 
---
+++ src/global.js
@@ -0,0 +1,3 @@
+ const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3004';
+
+ export {BACKEND_URL};

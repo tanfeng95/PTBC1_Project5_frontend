7fba34c9ba0ee31b3037a95f996ba79943f7bdb2
makck <joel.mak32@gmail.com>
2022-07-04 21:45:26 +0800

    Create button to add product

--- src/components/merchant/MerchantDashboard.jsx
+++ src/components/merchant/MerchantDashboard.jsx
@@ -23,7 +23,11 @@ export default function MerchantDashboard() {
       <div>
         <h1 className="font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600">Merchant Dashboard</h1>
       </div>
-
+      <div className="flex ml-3 justify-start">
+        <Link to="/merchant/product/add">
+          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add a new product</button>
+        </Link>
+      </div>
       <div>
         <ul className="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
           {shopProducts.map((p, index) => (

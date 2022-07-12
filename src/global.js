c99abc0c819aaa0ff528154fcb9c18568c208911
tanfeng95 <tanfeng95@hotmail.com>
2022-07-12 20:36:33 +0800

    add email to merchant

--- src/global.js
+++ src/global.js
@@ -1,7 +1,8 @@
   const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3004';
   const SALT = 'SALT';
   const serviceId = 'service_gw112me';
-  const templateId = 'template_csb16nn';
+  const customerTemplateId = 'template_csb16nn';
+  const merchantTemplateId = 'template_qvxz7wp';
   const userId = 'CzBOQBK_6d8Vwx0iT'
 
- export {BACKEND_URL, SALT,serviceId,templateId,userId};
+ export {BACKEND_URL, SALT,serviceId,customerTemplateId,merchantTemplateId,userId};

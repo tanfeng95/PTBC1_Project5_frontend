2b33c2c19c3a054ac9c3b5fd016cf4e53040c8df
makck <joel.mak32@gmail.com>
2022-07-13 01:09:49 +0800

    Add colourPalette

--- src/global.js
+++ src/global.js
@@ -1,7 +1,10 @@
-  const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3004';
-  const SALT = 'SALT';
-  const serviceId = 'service_gw112me';
-  const templateId = 'template_csb16nn';
-  const userId = 'CzBOQBK_6d8Vwx0iT'
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3004';
+const SALT = 'SALT';
+const serviceId = 'service_gw112me';
+const templateId = 'template_csb16nn';
+const userId = 'CzBOQBK_6d8Vwx0iT';
+const colourPalette = ['#ddd8c4', '#cfd5bd', '#c0d1b6', '#a3c9a8', '#94bfa4', '#84b59f', '#77ac9b', '#69a297', '#5d9193', '#50808e'];
 
- export {BACKEND_URL, SALT,serviceId,templateId,userId};
+export {
+  BACKEND_URL, SALT, serviceId, templateId, userId, colourPalette,
+};

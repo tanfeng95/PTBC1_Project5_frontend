cbf5d24045a24ebf837bff56f5744b9db9a554d6
makck <joel.mak32@gmail.com>
2022-07-13 02:03:56 +0800

    Add MetricSummary to dashboard

--- src/components/merchant/MerchantDashboard.jsx
+++ src/components/merchant/MerchantDashboard.jsx
@@ -7,10 +7,11 @@ import {
 import {
   Chart as ChartJS, ArcElement, Tooltip, Legend,
 } from 'chart.js';
-import { BACKEND_URL, colourPalette } from '../../global';
+import { BACKEND_URL } from '../../global';
 import MerchantNavBar from './MerchantNavBar';
 import SalesVolumeChart from './SalesVolumeChart';
 import RevenueChart from './RevenueChart';
+import MetricSummary from './MetricSummary';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -31,6 +32,9 @@ export default function MerchantDashboard() {
       <div>
         <h1 className="font-medium leading-tight text-5xl mt-0 mb-4 text-blue-600">Merchant Dashboard</h1>
       </div>
+
+      <MetricSummary />
+
       <div className="grid grid-cols-2 gap-2 px-8">
         <RevenueChart />
         <SalesVolumeChart />
